b2380f69a2d242994b80fc21a33c847466b8b5c3
My Name <abbdrhm@gmail.com>
2022-10-28 17:40:44 +0300

    romanToInt

--- romanToInt/src/App.java
+++ romanToInt/src/App.java
@@ -1,22 +1,76 @@
 import java.util.*;
 public class App {
-    public static void romanToInt(String roman){
+    public static void romanToInt(char roman[]){
         int total = 0;
-        switch(roman.charAt(0)){
-            case 'I': total+= 1;
-            case 'V': total+= 5;
-            case 'L': total+= 50;
-            case 'C': total+= 100;
-            case 'D': total+= 500;
-            case 'M': total+= 1000;
-        }
+        for(int i = 0; i < roman.length; i++){
+            
+            if(roman[i] == 'I')
+                total+=1;
+            else if (roman[i]== 'V')
+                {
+                    total+=5;
+                    if(i> 0 && roman[i-1]=='I')
+                    {
+                        total-= 2;
+                    }
+                        
+                }
+            else if (roman[i]== 'X')
+                {
+                    total+=10;
+                    if(i> 0 && roman[i-1]=='I')
+                    {
+                        total-= 2;
+                    }
+                    
+                        
+                }
+            
+            else if (roman[i]== 'L')
+                {
+                    total+=50;
+                    if(i> 0 && roman[i-1]=='X')
+                    {
+                        total-= 20;
+                    }
+
+                }
+            else if (roman[i]== 'C')
+            { 
+                total+=100; 
+                if(i> 0 && roman[i-1]=='X')
+                {
+                    total-= 20;
+                }
+                
+            }
+            else if (roman[i]== 'D')
+            {
+                total+=500;
+                if(i> 0 && roman[i-1]=='C')
+                {
+                    total-= 200;
+                }
+            }
+            else if (roman[i]== 'M')
+            {
+                total+=1000;
+                if(i> 0 && roman[i-1]=='C')
+                {
+                    total-= 200;
+                }
+                
+            }  
+        
+        }   
         System.out.println(total);
         
     }
     public static void main(String[] args) throws Exception {
         try (Scanner sc = new Scanner(System.in)) {
             String input = sc.nextLine();
-            romanToInt(input);
+            char [] num = input.toCharArray();
+            romanToInt(num);
         }
 
     }
